48ddef6693e102f39658367b9de36fdb049433ca
Justin Mueller <justin.mueller@icinga.com>
2020-05-29 15:05:18 +0200

    add parameter c
    refs #5

--- main.go
+++ main.go
@@ -13,11 +13,18 @@ func main() {
 	uniqueParameter := flag.Bool("u", false, "allows you to print only unique lines.")
 	repeatParameter := flag.Bool("d", false, "only print duplicate lines, one for each group.")
 	caseParameter := flag.Bool("i", false, "ignore differences in case when comparing.")
+	countParameter := flag.Bool("c", false, "prefix lines by the number of occurrences.")
 	flag.Parse()
 
 	buf := bufio.NewReader(os.Stdin)
 	checkEquality := []byte("")
 	skip := false
+	var count int
+	if *repeatParameter {
+		count = 0
+	} else {
+		count = 1
+	}
 
 	for {
 		content, readErr := buf.ReadBytes('\n')
@@ -40,11 +47,16 @@ func main() {
 				if readErr == io.EOF {
 					break
 				}
+				count++
 				continue
 			} else if bytes.Compare(content,checkEquality) != 0 {
 				if skip {
 					skip = false
+					if *countParameter {
+						fmt.Printf("      %d ", count)
+					}
 					_, _ = os.Stdout.Write(checkEquality)
+					count = 1
 				}
 				checkEquality = content
 				if readErr == io.EOF {
@@ -54,7 +66,11 @@ func main() {
 			} else if bytes.Compare(content, checkEquality) == 0 {
 				skip = true
 				checkEquality = content
+				count++
 				if readErr == io.EOF {
+					if *countParameter {
+						fmt.Printf("      %d ", count)
+					}
 					_, _ = os.Stdout.Write(checkEquality)
 					break
 				}
@@ -78,21 +94,41 @@ func main() {
 			} else if bytes.Compare(content, checkEquality) != 0 {
 				if skip {
 					skip = false
+					if *countParameter {
+						fmt.Printf("      %d ", count)
+					}
 					_, _ = os.Stdout.Write(checkEquality)
 				}
 				checkEquality = content
 				skip = true
 				if readErr == io.EOF {
+					_, _ = os.Stdout.Write(content)
 					break
 				}
 				continue
 			}
 		} else {
-			if bytes.Compare(checkEquality, content) != 0 {
-				_, _ = os.Stdout.Write(content)
-			}
-			if readErr == io.EOF {
-				break
+			if *countParameter {
+				if bytes.Compare(checkEquality, []byte("")) == 0 {
+					checkEquality = content
+					if readErr == io.EOF {
+						break
+					}
+					continue
+				}
+				if bytes.Compare(checkEquality, content) != 0 {
+					fmt.Printf("      %d ", count)
+					_, _ = os.Stdout.Write(checkEquality)
+					count = 0
+				}
+				count++
+			} else {
+				if bytes.Compare(checkEquality, content) != 0 {
+					_, _ = os.Stdout.Write(content)
+				}
+				if readErr == io.EOF {
+					break
+				}
 			}
 		}
 
